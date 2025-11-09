cad5c06a825d0eabda6d85c954673f42ee7f0abf
mgarcia33266 <michael.garcia@student.northampton.edu>
2025-11-09 17:29:04 +0000

    Complete.

--- GuessingGame.java
+++ GuessingGame.java
@@ -3,13 +3,30 @@
 
 public class GuessingGame {
   public static void main(String[] args) {
-    //Prompt for input
+    
+    Random random = new Random();
+    int number = random.nextInt(100) + 1;
+    System.out.println(number);
+  
 
-    //Read Input
-      
-    //convert using division and the remainder operator %
-     
-    //output the conversion
+    int randomInt = random.nextInt();
+        System.out.println("Random Integer: " + randomInt);
+
+
+      int randomIntBounded = random.nextInt(100);
+      System.out.println("Random Integer (0-99): " + randomInt);
+
+      int min = 1;
+      int max = 100;
+      int randomIntInRange = random.nextInt(max - min + 1) + min;
+      System.out.println("Random Integer (" + min + "-" + max + "): " + randomIntInRange);
+
+      double randomDouble = random.nextDouble();
+      System.out.println("Random Double: " + randomDouble);
+    
+     boolean randomBoolean = random.nextBoolean();
+     System.out.println("Random Boolean: " + randomBoolean);
+  
     
     
    }
